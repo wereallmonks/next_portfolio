17e3886d76c5f77d073bcbb77180e1a6bfa47368
Claudia <44302167+wereallmonks@users.noreply.github.com>
2022-01-28 11:48:34 -0800

    Enable CORS in app

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ];
+  },
 };
